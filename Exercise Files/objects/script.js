65583efb6f1954c82b59c51bfe0792cb930cabc5
jeffmlb91 <jeffmlb91@gmail.com>
2022-02-24 19:52:28 -0500

    added switch tires features

--- Exercise Files/objects/script.js
+++ Exercise Files/objects/script.js
@@ -8,45 +8,66 @@ const carObj = {
     seat: "Red",
     engine: 3.5,
   },
+  tires: {
+    front: "Winter-Tires",
+    rear: "Summer-Tires",
+  },
+  switchTires: function (new1, new2) {
+    this.tires.front = new1;
+    this.tires.rear = new2;
+  },
 };
 
+console.log("This car has:", carObj.tires);
+carObj.switchTires("Pirelli", "Michellin");
+console.log("Rear front tires has:", carObj.tires.front);
+console.log("Rear front tires has:", carObj.tires.rear);
+
 const houseObj = {
-    type: "Detached",
-    room: 5,
-    garage: {
-        numberOfLots: 3,
-        carList: {
-            make1: "BMW",
-            make2: "Mercedes",
-            make3: "Range-Rover"
-        },
-        tools: {
-            gardenTools: {
-                lawnmower: {
-                    honda: 1,
-                    yamaha: 1
-                },
-                trimmers: {
-                    honda: 1
-                },
-                hoses: "100 Meters long"
-            },
-            carTools: {
-                wheels: "4 sets of winter tires",
-                washingProducts: "Cleaning products"
-            }
-        },    
+  type: "Detached",
+  room: 5,
+  garage: {
+    numberOfLots: 3,
+    carList: {
+      make1: "BMW",
+      make2: "Mercedes",
+      make3: "Range-Rover",
     },
-    kitchen: {
-        island: true,
-        stove: true
-    }
-}
+    tools: {
+      gardenTools: {
+        lawnmower: {
+          honda: 1,
+          yamaha: 1,
+        },
+        trimmers: {
+          honda: 1,
+        },
+        hoses: "100 Meters long",
+      },
+      carTools: {
+        wheels: "4 sets of winter tires",
+        washingProducts: "Cleaning products",
+      },
+    },
+  },
+  kitchen: {
+    island: true,
+    stove: true,
+    highChair: false,
+    applyFurniture: function (status) {
+      this.highChair = status;
+      this.island = status;
+      this.stove = status;
+    },
+  },
+};
+// console.log("House's Kitchen highChair:", houseObj.kitchen.stove);
 
-console.log("This is the house Obj:", houseObj);
-console.log("House garge Tools: ", houseObj.garage.tools.carTools.wheels);
+// console.log("This is the house Obj:", houseObj);
+// console.log("House garage Tools: ", houseObj.garage.tools.carTools.wheels);
 
-console.log(houseObj.kitchen)
+// console.log(houseObj.kitchen);
+// console.log(houseObj["garage"])
 //console.log(carObj);
 // console.log("This is the car object:", carObj);
 // console.log("This is the year:", carObj.year);
