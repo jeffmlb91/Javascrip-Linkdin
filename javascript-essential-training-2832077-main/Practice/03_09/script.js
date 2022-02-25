29099f01d433b7996452189ef26f541970d2865a
jeffmlb91 <jeffmlb91@gmail.com>
2022-02-24 21:14:52 -0500

    updated Practice folder

--- javascript-essential-training-2832077-main/Practice/03_09/script.js
+++ javascript-essential-training-2832077-main/Practice/03_09/script.js
@@ -16,11 +16,39 @@ const backpack = {
     left: 26,
     right: 26,
   },
+
+  //defining a method to change the name property;
+
+  changeName: function(newName) {
+    this.name = newName
+  },
+
+  // Defining the  volume methode to change the volume property
+  changeVolume: function(newVolume) {
+    this.volume = newVolume;
+  },
+
+  changeColor: function(newColor) {
+    this.color = newColor;
+  },
+  
   toggleLid: function (lidStatus) {
     this.lidOpen = lidStatus;
   },
   newStrapLength: function (lengthLeft, lengthRight) {
     this.strapLength.left = lengthLeft;
     this.strapLength.right = lengthRight;
-  },
+  }
 };
+
+console.log("Old Name is:", backpack.name);
+backpack.changeName('Everynight Backpack');
+console.log("New Name is:", backpack.name);
+
+console.log("Old Volume is:", backpack.volume);
+backpack.changeVolume(500000)
+console.log("Old Volume is:", backpack.volume);
+
+console.log("Old color is:", backpack.color);
+backpack.changeColor('Pink');
+console.log("New color is:", backpack.color);
