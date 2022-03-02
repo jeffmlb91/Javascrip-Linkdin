38e472a39752c6b4fd25182e0dcff2bbbb286f79
jeffmlb91 <jeffmlb91@gmail.com>
2022-03-01 23:23:15 -0500

    Completed Class self excercise

--- Exercise Files/object_class/Person.js
+++ Exercise Files/object_class/Person.js
@@ -3,27 +3,41 @@
 // Class expression const Backpack = Class {}
 
 const Person = class {
-    constructor (
-        gender,
-        height,
-        favouriteFood,
-        workPlace,
-        school,
-        make,
-        model,
-        houseBig
-    ) {
-        this.gender = gender;
-        this.height = height;
-        this.favouriteFood = favouriteFood;
-        this.workPlace = workPlace;
-        this.school = school;
-        this.car = {
-            make: make,
-            model: model,
-        };
-        this.houseBig = houseBig;
-    }
-}
+  constructor(
+    gender,
+    height,
+    favouriteFood,
+    workPlace,
+    school,
+    make,
+    model,
+    houseBig
+  ) {
+    this.gender = gender;
+    this.height = height;
+    this.favouriteFood = favouriteFood;
+    this.workPlace = workPlace;
+    this.school = school;
+    this.car = {
+      make: make,
+      model: model,
+    };
+    this.houseBig = houseBig;
+  }
+  //Add methods like normal functions:
+  changeFavouriteFood(newFood) {
+    this.favouriteFood = newFood;
+  }
+//   changeCar() {
+//     this.car.make = newMake;
+//     this.car.model = newModel;
+//   }
+
+// We can user methods either ways like above this  comment and after it.
+  changeCar(newMake, newModel) {
+    this.car.make = newMake;
+    this.car.model = newModel;
+  }
+};
 
 export default Person;
--- Exercise Files/object_class/script.js
+++ Exercise Files/object_class/script.js
@@ -1,4 +1,8 @@
-import Backpack from "./Person.js";
+/**
+ * @link http://developer.mozilla.org/en-US/Web/Javascript/Reference/classes
+ **/
+
+import Person from "./Person.js";
 
 const firstPerson = new Person(
     "male",
@@ -13,3 +17,12 @@ const firstPerson = new Person(
 );
 
 console.log("Characteristics:", firstPerson);
+// console.log("The Gender is:", firstPerson.gender);
+// console.log("The Gender is:", firstPerson.workPlace);
+// console.log("The Gender is:", firstPerson.school);
+// console.log("The Gender is:", firstPerson.favouriteFood);
+firstPerson.changeFavouriteFood("rice");
+// firstPerson.car.make = "Audi"; This is assigning the value directly
+// console.log("New Favourite Food is:", firstPerson);
+firstPerson.changeCar("Ferrari", "F4");
+console.log("This is the new Person model:", firstPerson);
