7d87fc263ae22c53852f3bfd0d81300620d37642
jeffmlb91 <jeffmlb91@gmail.com>
2022-03-03 23:59:07 -0500

    udpated

--- javascript-essential-training-2832077-main/03_10/Backpack.js
+++ javascript-essential-training-2832077-main/03_10/Backpack.js
@@ -14,7 +14,7 @@ class Backpack {
     pocketNum,
     strapLengthL,
     strapLengthR,
-    lidOpen
+    lidOpen,
   ) {
     // Define properties:
     this.name = name;
@@ -35,6 +35,7 @@ class Backpack {
     this.strapLength.left = lengthLeft;
     this.strapLength.right = lengthRight;
   }
+
 }
 
 export default Backpack;
