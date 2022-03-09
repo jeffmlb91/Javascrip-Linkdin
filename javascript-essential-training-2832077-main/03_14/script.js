b15dbd52d5ed5e3733d5dc6e254bfbccf0c9f57f
jeffmlb91 <jeffmlb91@gmail.com>
2022-03-09 00:27:37 -0500

    add template literal 03_14

--- javascript-essential-training-2832077-main/03_14/script.js
+++ javascript-essential-training-2832077-main/03_14/script.js
@@ -14,15 +14,23 @@ import Book from "./Book.js";
 const everyday = new Backpack(
   "Everyday Backpack",
   30,
-  "grey",
+  "Grey",
   15,
   26,
   26,
   false,
-  "December 5, 2018 15:00:00 PST"
+  "February 5, 2021 13:00:00 PST"
 );
 
-const firstBookRelease = new Book("Bible", "YHWH", 0, "Israel", "Believe", true, "December 5, 2018 15:00:00 PST");
+const firstBookRelease = new Book(
+  "Bible",
+  "YHWH",
+  2017,
+  "Israel",
+  "Believe",
+  true,
+  "December 5, 2018 15:00:00 PST"
+);
 
 const secondBookRelease = new Book(
   "Becoming",
@@ -64,9 +72,48 @@ const fithBookRelease = new Book(
   "December 5, 2018 15:00:00 PST"
 );
 
+const myStore = `
+<maim>
+<maim>
+<h1>Book Store</h1>
+<h2>What defines a book:</h2>
+<ul>
+  <li>Name: ${firstBookRelease.name}</li>
+  <li>Author: ${firstBookRelease.author}</li>
+  <li>Published: ${firstBookRelease.release.year}</li>
+  <li>Country: ${firstBookRelease.release.location}</li>
+  <li>Genre: ${firstBookRelease.genre}</li>
+  <li>Availability: ${firstBookRelease.availability}</li>
+  <li>date Acquired: ${firstBookRelease.dateAcquired}</li>
+  <li>Number of days since released: ${firstBookRelease.rentTime()} days</li>
+</ul>
+
+<section>
+  <h2>This is what a Backpack contains: </h2>
+  <ul>
+    <li>Name: ${everyday.name}</li>
+    <li>Volume: ${everyday.volume}</li>
+    <li>Color: ${everyday.color}</li>
+    <li>Number of pocket: ${everyday.pocketNum}</li>
+    <li>Strap Length Left Side: ${everyday.strapLength.left}</li>
+    <li>Strap Length Right Side: ${everyday.strapLength.right}</li>
+    <li>lid status: ${everyday.lidOpen}</li>
+    <li>date Acquired: ${everyday.dateAcquired}</li>
+    <li>Number of days since released: ${everyday.backpackAge()} days</li>
+  </ul>
+</section>
+</maim>
+
+`
+document.body.innerHTML = myStore;
+
+
+
 // console.log("The everydayPack object:", everydayPack);
 // console.log("The pocketNum value:", everydayPack.pocketNum);
 // console.log("Days since aquired:", everydayPack.backpackAge());
+
+
 
 console.log(
   "This are all the books:",
