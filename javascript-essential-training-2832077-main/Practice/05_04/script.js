9ac4110a7c4fc1d8202f49a701da926723cccf18
jeffmlb91 <jeffmlb91@gmail.com>
2022-05-12 01:18:08 -0400

    manupulated DOM with li elements

--- javascript-essential-training-2832077-main/Practice/05_04/script.js
+++ javascript-essential-training-2832077-main/Practice/05_04/script.js
@@ -9,12 +9,32 @@
 // console.log(allClass);
 // const allLiColor = allClass.forEach(item => item.style.backgroundColor = "blue");
 
+// changing the h1 tag name and background color and text color
 const newTitle = document.querySelector("h1");
 newTitle.textContent = "AllDAy BackPack";
 newTitle.style.backgroundColor = "Yellow";
+newTitle.style.color = "Turquoise";
 
+// changing ul background color
 const targetLiElement = document.querySelector("ul");
 targetLiElement.style.backgroundColor = "orange";
 
-const targetLiSpecEl = document.getElementByID
+// changing li element background color;
+const targetLiSpecElFirst = document.querySelector("li:nth-child(1)");
+targetLiSpecElFirst.style.backgroundColor = "blue";
 
+const targetListSpecElSecond = document.querySelector("li:nth-child(2)");
+targetListSpecElSecond.textContent = "Color: Green";
+targetListSpecElSecond.style.backgroundColor = "Green";
+
+const targetListSpecElThird = document.querySelector("li:nth-child(3)");
+targetListSpecElThird.style.backgroundColor = "Beige";
+targetListSpecElThird.textContent = "Age: 80 years old";
+
+const targetNumOfPocket = document.querySelector("li:nth-child(4)");
+targetNumOfPocket.textContent = "Number of Pockets: 75";
+targetNumOfPocket.style.backgroundColor = "tomato";
+
+// changed first li text content to 80L
+const targetLiSpecText = document.querySelector("li:nth-child(1)");
+targetLiSpecText.textContent = "volume: 80L";
