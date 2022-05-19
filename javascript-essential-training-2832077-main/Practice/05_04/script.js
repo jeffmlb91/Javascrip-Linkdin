0d1a0a08f6777b503fec3f446f9f71c1a8ec47d5
jeffmlb91 <jeffmlb91@gmail.com>
2022-05-18 23:24:03 -0400

    added attribute and replaced existing one

--- javascript-essential-training-2832077-main/Practice/05_04/script.js
+++ javascript-essential-training-2832077-main/Practice/05_04/script.js
@@ -40,66 +40,72 @@ const targetLiSpecText = document.querySelector("li:nth-child(1)");
 targetLiSpecText.textContent = "volume: 80L";
 
 // TARGET CLASSES WITH JAVASCRIPT
-const targetClassParent = document.querySelector('h1').classList;
+const targetClassParent = document.querySelector("h1").classList;
 console.log(targetClassParent);
-const getClassList = document.querySelector('main li:nth-child(2)').classList;
-console.log('New Value', getClassList);
+const getClassList = document.querySelector("main li:nth-child(2)").classList;
+console.log("New Value", getClassList);
 
-//TARGET CLASSES THEN ADD A NEW CLASS WITH THE 
-
+//TARGET CLASSES THEN ADD A NEW CLASS WITH THE
 
 // CLASS.ADD
 
-const addClass = document.querySelector('main h1');
-console.log('This is', addClass)
-addClass.classList.add('first__tag__class');
-addClass.classList.add('this_is_cool');
-
+const addClass = document.querySelector("main h1");
+console.log("This is", addClass);
+addClass.classList.add("first__tag__class");
+addClass.classList.add("this_is_cool");
 
 //CLASS.REMOVED
-const removeClass = document.querySelector('main article li:nth-child(2)');
-const removeOtherClass = document.querySelector('main li:nth-child(3)')
-console.log("additional", removeOtherClass)
+const removeClass = document.querySelector("main article li:nth-child(2)");
+const removeOtherClass = document.querySelector("main li:nth-child(3)");
+console.log("additional", removeOtherClass);
 console.log(removeClass);
 removeClass.classList.remove("feature");
-removeClass.classList.add('brand__new__lisyt')
-removeClass.classList.add("spinach__class")
+removeClass.classList.add("brand__new__lisyt");
+removeClass.classList.add("spinach__class");
 
 //CLASS.REPLACE
 
-const replaceClass = document.querySelector('main li:nth-child(1)');
+const replaceClass = document.querySelector("main li:nth-child(1)");
 console.log(replaceClass);
-replaceClass.classList.replace('backpack__volume', 'backpack__mass')
-
+replaceClass.classList.replace("backpack__volume", "backpack__mass");
 
 //Class.toggle : USED TO DISPLAY OR HIDE
 replaceClass.classList.toggle("backpack__volume");
 replaceClass.classList.toggle("backpack__volume");
 
-
 //checking for all attributes an element contains
 // .attributes
 
-const imageAttribute = document.querySelector('img').attributes;
+const imageAttribute = document.querySelector("img").attributes;
 console.log(imageAttribute);
-
 
 //checking a specific attribute we want to find inside the element
 //.hasAttributes
-const imageHasAttributeSrc = document.querySelector('img').hasAttribute('src');
+const imageHasAttributeSrc = document.querySelector("img").hasAttribute("src");
 console.log(imageHasAttributeSrc); // return a boolean true
 
-const imageHasAttributeAlt = document.querySelector('img').hasAttribute('alt');
+const imageHasAttributeAlt = document.querySelector("img").hasAttribute("alt");
 console.log(imageHasAttributeAlt);
-
 
 //setting or creating attribute for an elements
 //.createAttribute .setAttribute
-const imageSetAttribute = document.querySelector("img").setAttribute('class', 'do not add');
+const imageSetAttribute = document
+  .querySelector("img")
+  .setAttribute("class", "do not add");
 console.log(imageSetAttribute); // attribute listed on .attribute
 
 //removing an existing attribute from the element
 //.removeAttribute
 
-const removeAttribute = document.querySelector('img').removeAttribute('class');
-console.log(removeAttribute); // attribute listed on .attribute
+const removeAttribute = document.querySelector("img").removeAttribute("class");
+console.log(removeAttribute);
+
+//quick test
+
+const checkElementValue = document.querySelector("h1").attributes;
+const checkElementValueNew = document.querySelector("h1").setAttribute('alt', "alternative");
+const checkElementValueNewRemoved = document.querySelector('h1').setAttribute('alt', 'newattribute added')
+
+console.log("Hey", checkElementValue);
+console.log("checkElementValueNew", checkElementValueNew);
+console.log(checkElementValueNewRemoved);
