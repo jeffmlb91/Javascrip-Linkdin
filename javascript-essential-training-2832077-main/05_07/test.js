597f98d3acc948cfab7ceae1f846598098b0578a
jeffmlb91 <jeffmlb91@gmail.com>
2022-05-19 23:38:52 -0400

    learn in-line style with DOM

---
+++ javascript-essential-training-2832077-main/05_07/test.js
@@ -0,0 +1,12 @@
+// const styleDom = document.querySelector(".siteheader");
+// console.log(styleDom);
+const styleDom = document.querySelector(".siteheader").style;
+console.log(styleDom);
+// const styleDomFirstTag = document.querySelector(".site-title").style;
+// console.log(styleDomFirstTag)
+
+//change the font color of the header
+const styleDomFirstTag = document.querySelector(".site-title").style.color;
+console.log(styleDomFirstTag); // returns black in the console
+
+
