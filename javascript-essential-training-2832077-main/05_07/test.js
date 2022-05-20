0e4cf731e408b359e67ecb2d0caca8c61929351f
jeffmlb91 <jeffmlb91@gmail.com>
2022-05-19 23:57:47 -0400

    learn in line style DOM

--- javascript-essential-training-2832077-main/05_07/test.js
+++ javascript-essential-training-2832077-main/05_07/test.js
@@ -12,3 +12,8 @@ console.log(styleDomFirstTag);
 //change the font color of the header
 const styleDomFirstTagColor = document.querySelector(".site-description").style.color = "rebeccapurple";
 console.log(styleDomFirstTagColor);
+
+// add background color to the header
+const addBackgroundColor = document.querySelector(".site-title").style.backgroundColor = "pink";
+console.log(addBackgroundColor);
+console.log(document.querySelector(".site-title").style); // display result in console.log()
